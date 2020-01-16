d1dc9d2932b38961b629a18f8aac81e8008c51c8
laser <l@s3r.me>
2020-01-16 08:10:49 -0800

    add comments and pseudocode outlining how we'll get the state we need

--- internal/app/go-filecoin/internal/submodule/piece_manager_submodule.go
+++ internal/app/go-filecoin/internal/submodule/piece_manager_submodule.go
@@ -68,9 +68,109 @@ func (s *ChainBackedPieceManager) SealPieceIntoNewSector(ctx context.Context, de
 }
 
 func (s *ChainBackedPieceManager) UnsealSector(sectorId uint64) (io.Reader, error) {
-	s.builder.ReadPieceFromSealedSector(sectorId, 0, s.builder.SectorSize())
+	// - let ID = `sectorId`
+	// - get slice of pre-committed sector info PSIS from storage miner actor storage
+	// - let PSI = nil
+	// - for each p in PSIS: if p.SectorNumber.ID == ID then { PSI = p; break }
+	// - let TICKET = p.Ticket, COMMD = p.CommD
+	ticket := []byte{}
+	commD := []byte{}
+
+	return s.builder.ReadPieceFromSealedSector(sectorId, 0, s.builder.SectorSize(), ticket, commD)
 }
 
 func (s *ChainBackedPieceManager) LocatePieceWithinSector(dealID uint64) (sectorID uint64, offset uint64, length uint64, err error) {
-	panic("implement me")
+	/*
+
+		// how the storage market actor structures sector id -> []DealID mapping
+
+		type DealID int64
+		type DealIDs struct {
+			Items []DealID
+		}
+
+		type SectorPreCommitInfo struct {
+		    SectorNumber  abi.SectorNumber
+		    SealedCID     abi.SealedSectorCID  // CommR
+		    SealEpoch     abi.ChainEpoch
+		    DealIDs       abi.DealIDs
+		    Expiration    abi.ChainEpoch
+		}
+
+		// where and how does the storage miner actor store SectorPreCommitInfo
+
+		func (a *StorageMinerActorCode_I) PreCommitSector(rt Runtime, info SectorPreCommitInfo) {
+			h, st := a.State(rt)
+			rt.ValidateImmediateCallerIs(st.Info().Worker())
+
+			if _, found := st.Sectors()[info.SectorNumber()]; found {
+				rt.AbortStateMsg("Sector number already exists in table")
+			}
+
+			...
+
+			newSectorInfo := &SectorOnChainInfo_I{
+				State_:            SectorState_PreCommit,
+				Info_:             info,
+				PreCommitDeposit_: depositReq,
+				PreCommitEpoch_:   rt.CurrEpoch(),
+				ActivationEpoch_:  epochUndefined,
+				DealWeight_:       *big.NewInt(-1),
+			}
+			st.Sectors()[info.SectorNumber()] = newSectorInfo
+
+			...
+		}
+
+		// getting a deal from the storage market actor
+
+		func (st *StorageMarketActorState_I) _getOnChainDeal(dealID abi.DealID) (
+			deal OnChainDeal, dealP StorageDealProposal, ok bool) {
+
+			var found bool
+			deal, found = st.Deals()[dealID]
+			if found {
+				dealP = deal.Deal().Proposal()
+			} else {
+				deal = nil
+				dealP = nil
+			}
+			return
+		}
+
+		type StorageDealProposal struct {
+			PieceCID                      abi.PieceCID  // CommP
+			PieceSize                     abi.PieceSize
+
+			...
+		}
+
+		type StorageDeal struct {
+			Proposal  StorageDealProposal
+			CID()     &StorageDeal
+		}
+
+		type OnChainDeal struct {
+			ID                abi.DealID
+			Deal              StorageDeal
+			...
+		}
+
+
+
+	*/
+
+	// TODO:
+	//
+	// - let ID = `dealID`
+	// - get slice of pre-committed sector info PSIS from storage miner actor storage
+	// - let PSI = nil
+	// - for each p in PSIS: for each d in p.DealIDs: if d.ID == ID then { PSI = p; break }
+	// - for each d in PSI.DealIDs, call StorageMarketActor#_getOnChainDeal(d) to build a slice of OnChainDeal OS
+	// - let DP = nil
+	// - let sum = 0;
+	// - for each o in OS: if o.ID == ID then { break } else { sum += o.Deal.Proposal.PieceSize }
+	// - return (PSI.SectorNumber, sum, DP.PieceSize)
+
+	return 0, 0, 0, nil
 }
